4e534937105e71aba25ca35854bfa3ebccb4ded7
HarshaKy <harshaky6573@gmail.com>
2020-06-29 15:09:02 +0530

    added stuff

---
+++ server.js
@@ -0,0 +1,54 @@
+require('isomorphic-fetch')
+const dotenv = require('dotenv')
+const Koa = require('koa')
+const next = require('next')
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth')
+const { verifyRequest } = require('@shopify/koa-shopify-auth')
+const session = require('koa-session')
+
+dotenv.config()
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const { apikey, apiSecretKey } = process.env
+
+app.prepare().then(() => {
+    const server = new Koa()
+    server.use(session({ secure: true, sameSite: 'none' }, server))
+    server.keys = [apiSecretKey]
+    
+    server.use(
+        createShopifyAuth({
+			apiKey: apikey,
+			secret: apiSecretKey,
+			scopes: ['read_products'],
+			afterAuth(ctx) {
+				const { shop, accessToken } = ctx.session;
+				ctx.cookies.set('shopOrigin', shop, {
+					httpOnly: false,
+					secure: true,
+					sameSite: 'none'
+				});
+				ctx.redirect('/')
+          	},
+        }),
+      )
+
+    server.use(verifyRequest())
+
+    server.use(async (ctx) => {
+        await handle(ctx.req, ctx.res)
+        ctx.respond = false
+        ctx.res.statusCode = 200
+        return
+    })
+    
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+})
+
+
